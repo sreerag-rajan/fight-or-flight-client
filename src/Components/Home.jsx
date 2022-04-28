c3cdaa71175f2f95843ace828fed7cda26e18a4f
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-04-28 19:23:32 +0530

    applied a basic search functionality for finding flights between two airports

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,49 +1,90 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { getFlightsData } from '../Redux/flights/flights.action'
+import { getFlightsData, setFlights } from '../Redux/flights/flights.action'
+import {Table, Thead, Tbody, Tr, Th, Td, TableContainer, Select, Flex, Button} from '@chakra-ui/react'
+import axios from 'axios'
 
 
 export const Home = ()=>{
     const flights = useSelector((store)=>store.flights.flights)
     const dispatch = useDispatch();
+    const [airports, setAirports] = useState([])
+    const [formData, setFormData] = useState({
+        start:"",
+        end:""
+    })
 
     useEffect(()=>{
         dispatch(getFlightsData())
+        axios.get("http://localhost:8080/airport").then((({data})=>{
+            setAirports(data);
+        }))
     },[])
+
+    const handleChange=(e)=>{
+        const {id, value} = e.target
+        setFormData({...formData, [id]:value})
+    }
+
+    const handleSearch = ()=>{
+        axios.get(`http://localhost:8080/flights/search?start=${formData.start}&end=${formData.end}`).then(({data})=>{
+            dispatch(setFlights(data));
+        })
+        .catch((er)=>{
+            console.log(er)
+        })
+    }
     return(
         <div>
             <h1>Fight or Flight</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Airline</th>
-                        <th>From</th>
-                        <th>To</th>
-                        <th>Start Date</th>
-                        <th>Start Time</th>
-                        <th>End Date</th>
-                        <th>End Time</th>
-                        <th>Cost</th>
-                        <th>Capacity</th>
-                    </tr>
-                </thead>
-                <tbody>
+            <div>
+                <Flex justifyContent={"center"} margin={"40px"} gap={"20px"}>
+                <Select onChange={handleChange} value={formData.start} width={"300px"} placeholder="From" size="sm" name="start" id="start">
+                    {airports?.map((el)=>{
+                        return <option value={el._id}>{el.code}-{el.name}</option>
+                    })}
+                </Select>
+                <Select onChange={handleChange} value={formData.end} width={"300px"} placeholder="To" size="sm" name="end" id="end">
+                    {airports?.map((el)=>{
+                        return <option value={el._id}>{el.code}-{el.name}</option>
+                    })}
+                </Select>
+                <Button onClick={handleSearch}>Search</Button>
+                </Flex>
+            </div>
+            <TableContainer>
+            <Table variant={"striped"}>
+                <Thead>
+                    <Tr>
+                        <Th>Airline</Th>
+                        <Th>From</Th>
+                        <Th>To</Th>
+                        <Th>Start Date</Th>
+                        <Th>Start Time</Th>
+                        <Th>End Date</Th>
+                        <Th>End Time</Th>
+                        <Th>Cost</Th>
+                        <Th>Capacity</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
                 {flights?.map((el,i)=>{
-                    return <tr key={el._id}>
-                        <td>{el.flightCompany}-{el.flightNumber}</td>
-                        <td>{el.startAirport.name}</td>
-                        <td>{el.endAirport.name}</td>
-                        <td>{new Date(el.startTime).toLocaleDateString()}</td>
-                        <td> {new Date(el.startTime).toLocaleTimeString()}</td>
-                        <td>{new Date(el.endTime).toLocaleDateString()}</td>
-                        <td>{new Date(el.endTime).toLocaleTimeString()}</td>
-                        <td>{el.cost}</td>
-                        <td>{el.capacity} kgs</td>
-                    </tr>
+                    return <Tr key={el._id}>
+                        <Td>{el.flightCompany}-{el.flightNumber}</Td>
+                        <Td>{el.startAirport.name}</Td>
+                        <Td>{el.endAirport.name}</Td>
+                        <Td>{new Date(el.startTime).toLocaleDateString()}</Td>
+                        <Td> {new Date(el.startTime).toLocaleTimeString()}</Td>
+                        <Td>{new Date(el.endTime).toLocaleDateString()}</Td>
+                        <Td>{new Date(el.endTime).toLocaleTimeString()}</Td>
+                        <Td isNumeric>{el.cost}</Td>
+                        <Td>{el.capacity} kgs</Td>
+                    </Tr>
                 })}
 
-                </tbody>
-            </table>
+                </Tbody>
+            </Table>
+            </TableContainer>
             
         </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './App'
 import {BrowserRouter} from "react-router-dom";
 import {Provider as ReduxProvider} from "react-redux"
 import {store} from "./Redux/store"
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <ReduxProvider store={store}>
-    <App />
+      <ChakraProvider>
+          <App />
+      </ChakraProvider>
     </ReduxProvider>
     </BrowserRouter>
   </React.StrictMode>
